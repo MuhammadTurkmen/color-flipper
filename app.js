aceaad963a5a5dbae4d96fc532b261137b78e048
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-05 20:12:25 +0430

    init

--- app.js
+++ app.js
@@ -3,4 +3,6 @@ const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 const btn = document.getElementById('btn')
 const color = document.querySelector('.color')
 
-btn.addEventListener('click')
+btn.addEventListener('click', function() {
+    
+})

b30c3f20b952d5e4ea148e1e65c46cca3d34afc2
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-05 08:41:05 +0430

    init

--- app.js
+++ app.js
@@ -9,3 +9,7 @@ btn.addEventListener('click', function() {
     document.body.style.backgroundColor = colors[randomNumber]
     color.textContent = colors[randomNumber]
 })
+
+function getRandomNumber() {
+    
+}

9c836eff508d0223d9b904ecfb2cbd46fb04dd6a
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-05 08:42:28 +0430

    init

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const color = document.querySelector('.color')
 
 btn.addEventListener('click', function() {
     // get random number between 0 - 3
-    const randomNumber = 2
+    const randomNumber = getRandomNumber()
     document.body.style.backgroundColor = colors[randomNumber]
     color.textContent = colors[randomNumber]
 })
